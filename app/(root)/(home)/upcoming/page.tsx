875d0b28939631dc1e21865e6fd1600a85c84521
deya19 <deyasaleh2@gmail.com>
2024-04-15 00:27:09 +0300

    Zoom application

---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,16 @@
+import CallList from '@/components/CallList'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+        UpComings
+      </h1>
+
+      <CallList
+        type="upcoming"
+      />
+    </section>
+  )
+}
